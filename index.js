443149b5f218bcb3639383c59ba899ba0fb76f28
Viraj Jain <harshal.jain.csai.20@ggits.net>
2024-08-06 00:01:59 +0530

    fix otp 2

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.post('/sendotp', async (req, res) => {
     const { phone } = req.body;
     const otp = Math.floor(1000 + Math.random() * 9000);
     const apiKey = process.env.FAST2SMS_API_KEY
-    let url = `https://www.fast2sms.com/dev/bulkV2?authorization=${apiKey}&route=otp&variables_values=${otp}&flash=${flash}&numbers=${phone}`
+    let url = `https://www.fast2sms.com/dev/bulkV2?authorization=${apiKey}&route=otp&variables_values=${otp}&flash=0&numbers=${phone}`
 
     console.log(url)
 
